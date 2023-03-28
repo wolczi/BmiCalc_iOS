af1dc56bca098df1c96237fa104cfc6ecc7ef38e
Wołczi <pwolczacki@gmail.com>
2023-03-28 16:37:18 +0200

    Initial Commit

---
+++ BmiCalc/BmiCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  BmiCalcApp.swift
+//  BmiCalc
+//
+//  Created by Przemysław Wołczacki on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BmiCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
