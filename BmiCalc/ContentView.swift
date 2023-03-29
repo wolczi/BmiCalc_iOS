815bbbbf177969ef5dba32166c7ddf419e39b184
Wołczi <pwolczacki@gmail.com>
2023-03-29 12:52:22 +0200

    add metric and imperial units

--- BmiCalc/ContentView.swift
+++ BmiCalc/ContentView.swift
@@ -10,76 +10,179 @@ import SwiftUI
 struct ContentView: View {
     
     @FocusState private var focusedField: FocusedField?
+    @State private var selectedUnit: Units = .metric
     
-    @State private var weightString = ""
-    @State private var heightString = ""
+    /* Metric Units */
+    @State private var kilogramsString = ""
+    @State private var centimetersString = ""
+    
+    /* Imperial Units */
+    @State private var feetString = ""
+    @State private var inchesString = ""
+    @State private var poundsString = ""
     
     @State private var result = ""
     @State private var diagnosis = ""
     
     init() {
+        UISegmentedControl.appearance().selectedSegmentTintColor = .systemBlue
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor : UIColor.white], for: .selected)
+        
         UITextField.appearance().clearButtonMode = .whileEditing
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor.systemBlue]
     }
     
     enum FocusedField {
         case weight, height
     }
     
+    enum Units: String, CaseIterable {
+        case metric = "Metric"
+        case imperial = "Imperial"
+    }
+    
+    func calculateBmi() {
+        
+        if selectedUnit == .metric {
+            if (centimetersString.isEmpty || kilogramsString.isEmpty) {
+                self.result = "Complete the fields!"
+                self.diagnosis = ""
+            }else {
+                let height = Double(centimetersString) ?? 0.0
+                let weight = Double(kilogramsString) ?? 0.0
+
+                let result = (weight/(height*height)) * 10000
+                let roundedResult = round(result * 100) / 100
+                
+                makeDiagnosis(roundedResult)
+            }
+        }else {
+            if (feetString.isEmpty || inchesString.isEmpty || poundsString.isEmpty) {
+                self.result = "Complete the fields!"
+                self.diagnosis = ""
+            }else {
+        
+                let feetToCm = (Double(feetString) ?? 0.0) * 30.48
+                let inchesToCm = (Double(inchesString) ?? 0.0) * 2.54
+                let poundsToKg = (Double(poundsString) ?? 0.0) * 0.45359237
+                
+                let height = feetToCm + inchesToCm
+                let weight = poundsToKg
+                
+                let result = (weight/(height*height)) * 10000
+                let roundedResult = round(result * 100) / 100
+                
+                makeDiagnosis(roundedResult)
+            }
+        }
+        
+    
+    }
+    
+    func makeDiagnosis(_ roundedResult: Double) {
+        self.result = "BMI = \(roundedResult)"
+        
+        switch roundedResult{
+            case 0..<18.5:
+                diagnosis = "Underweight"
+            case 18.5..<25:
+                diagnosis = "Normal weight"
+            case 25..<30:
+                diagnosis = "Overweight"
+            case 30...:
+                diagnosis = "Obesity"
+            default:
+                diagnosis = "???"
+        }
+    }
+    
+    func clearStrings() {
+        result = ""
+        diagnosis = ""
+        
+        kilogramsString = ""
+        centimetersString = ""
+        
+        feetString = ""
+        inchesString = ""
+        poundsString = ""
+    }
+    
     var body: some View {
         NavigationView{
             VStack {
+                
+                Picker("Choose units", selection: $selectedUnit) {
+                    ForEach(Units.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }
+                .onChange(of: selectedUnit, perform: { _ in
+                    clearStrings()
+                })
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
 
-                TextField("Enter your weight in kilograms", text: $weightString)
-                    .numbersOnly($weightString)
-                    .focused($focusedField, equals: .weight)
-                    .textFieldStyle(.roundedBorder)
-                    .padding()
                 
-                TextField("Enter your height in centimeters", text: $heightString)
-                    .numbersOnly($heightString)
-                    .focused($focusedField, equals: .height)
-                    .textFieldStyle(.roundedBorder)
-                    .padding()
+                    if(selectedUnit == .metric) {
+                        VStack {
+                            TextField("Centimeters", text: $centimetersString)
+                                .numbersOnly($centimetersString)
+                                .focused($focusedField, equals: .height)
+                                .textFieldStyle(.roundedBorder)
+                                .padding([.leading, .trailing])
+                            
+                            TextField("Kilograms", text: $kilogramsString)
+                                .numbersOnly($kilogramsString)
+                                .focused($focusedField, equals: .weight)
+                                .textFieldStyle(.roundedBorder)
+                                .padding()
+                        }
+                        
+                    }else {
+                        VStack {
+                            HStack {
+                                TextField("Feet", text: $feetString)
+                                    .numbersOnly($feetString)
+                                    .focused($focusedField, equals: .height)
+                                    .textFieldStyle(.roundedBorder)
+                                    .padding(.leading)
+                                
+                                TextField("Inches", text: $inchesString)
+                                    .numbersOnly($inchesString)
+                                    .focused($focusedField, equals: .height)
+                                    .textFieldStyle(.roundedBorder)
+                                    .padding(.trailing)
+                            }
+                                
+                            TextField("Pounds", text: $poundsString)
+                                .numbersOnly($poundsString)
+                                .focused($focusedField, equals: .height)
+                                .textFieldStyle(.roundedBorder)
+                                .padding()
+                        }
+                        
+                    }
                 
+            
                 if !result.isEmpty {
                     VStack{
                         Text(result)
                         Text(diagnosis)
                     }
-                        .padding()
+                    .padding()
                 }
                 
                 Button("Calculate") {
-                    if (heightString.isEmpty || weightString.isEmpty) {
-                        self.result = "Complete the fields!"
-                        
-                        self.diagnosis = ""
-                    }else {
-                        let height = Double(heightString) ?? 0.0
-                        let weight = Double(weightString) ?? 0.0
-                        let result = (weight/(height*height)) * 10000
-                        let roundedResult = round(result * 100) / 100
-                        
-                        self.result = "BMI = \(roundedResult)"
-                        
-                        switch roundedResult{
-                            case 0..<18.5:
-                                diagnosis = "Underweight"
-                            case 18.5..<25:
-                                diagnosis = "Normal weight"
-                            case 25...:
-                                diagnosis = "Overweight"
-                            default:
-                                diagnosis = "??"
-                        }
-                        
-                    }
-                
+                    focusedField = nil
+                    
+                    calculateBmi()
                 }
-                    .padding()
-                    .background(.blue)
-                    .foregroundColor(.white)
-                    .clipShape(Capsule())
+                .padding()
+                .background(.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
                 
                 Spacer()
             }
@@ -93,13 +196,28 @@ struct ContentView: View {
                     Button {
                         focusedField = nil
                         
-                        if(heightString.last == "." || heightString.last == ","){
-                            heightString += "0"
+                        if selectedUnit == .metric {
+                            if(centimetersString.last == "." || centimetersString.last == ","){
+                                centimetersString += "0"
+                            }
+
+                            if(kilogramsString.last == "." || kilogramsString.last == ","){
+                                kilogramsString += "0"
+                            }
+                        }else {
+                            if(feetString.last == "." || feetString.last == ","){
+                                feetString += "0"
+                            }
+
+                            if(inchesString.last == "." || inchesString.last == ","){
+                                inchesString += "0"
+                            }
+
+                            if(poundsString.last == "." || poundsString.last == ","){
+                                poundsString += "0"
+                            }
                         }
                         
-                        if(weightString.last == "." || weightString.last == ","){
-                            weightString += "0"
-                        }
                         
                     } label: {
                         Image(systemName: "keyboard.chevron.compact.down")
